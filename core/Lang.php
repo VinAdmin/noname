4a03a6d2566fadcaf163857b118125167ecc4139
Vtaliy <ovvitalik@gmail.com>
2019-10-02 15:33:30 +0500

    По умолчанию язык en-EN

--- core/Lang.php
+++ core/Lang.php
@@ -1,14 +1,24 @@
 <?php
+/**
+ * Описание класса: Языковой класс перевода.
+ *
+ * @package    NoName
+ * @subpackage Lang
+ * @author     Ольхин Виталий <volkhin@texnoblog.uz>
+ * @copyright  (C) 2019
+ */
+
 namespace app\core;
 
 class Lang
 {
+	//Переводы ядра
 	static function CoreTranslate()
 	{
 		global $config;
-		
+		//Если массива $config['lang'] по умолчанию выводится на английском языке
 		if(!isset($config['lang'])){
-			$config['lang'] = 'ru-RU';
+			$config['lang'] = 'en-EN';
 		}
 		
 		$pars = parse_ini_file('lang/'.$config['lang'].'/'.$config['lang'].'.ini');
