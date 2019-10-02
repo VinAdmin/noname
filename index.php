f79c49e6d79054c5a9b21b5fa3d6fff8905dddd6
Vtaliy <ovvitalik@gmail.com>
2019-10-02 14:05:36 +0500

    Добавлнена проверка на существование файла config.php

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 session_start();
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/functions/docroot.php';
+if(file_exists ( __DIR__ . '/config.php')){
+	require_once __DIR__ . '/config.php';
+}
+else{
+	$messag_config_file = "<div>Конфигурационный файл ненайден.</div>";
+	$messag_config_file .= "<div>Переименуйте example.config.php в config.php.</div>";
+	echo $messag_config_file;
+	exit;
+}
+
 
 use app\core\Route;
 use app\core\UpdateSystems;
