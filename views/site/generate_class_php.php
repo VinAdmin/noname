4fdb7c86831fd36879ddf87f02b02f1b4c05c18d
Vtaliy <ovvitalik@gmail.com>
2019-10-02 12:42:56 +0500

    значительных изменений нет

--- views/site/generate_class_php.php
+++ views/site/generate_class_php.php
@@ -1,10 +1,12 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12 col-sm-12 col-lg-12 ">
-			<h2 class="title-update">Форма контроля версий файлов</h2>
+			<h2 class="title-update">Форма созданеия класса</h2>
+			
 			<form name="generate_class">
 				<label>Имя класса</label>
 				<input name="class_name" id="class_name" value="">
+				<input type="submit" name="create" value="Создать">
 			</form>
 		</div>
 	</div>
